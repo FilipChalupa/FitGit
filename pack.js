edc51e0a95276633f9de1297257d9df9daee8d78
Filip Chalupa <chalupa.filip@gmail.com>
2016-11-15 22:29:01 +0100

    Mango: moved to root

--- pack.js
+++ pack.js
@@ -2,7 +2,6 @@ var asar = require('asar')
 /*var Mango = require('mango-cli')
 var MangoConfig = require('./node_modules/mango-cli/lib/helpers/config')*/
 
-var MANGO_DIR= 'views'
 
 console.log('Packing app')
 /*
@@ -16,14 +15,14 @@ var mangoProject = new Mango(MANGO_DIR, (new MangoConfig(MANGO_DIR)).get())
 var exec = require('child_process').exec
 
 // @TODO: better `mango install`
-exec('node ../node_modules/mango-cli/bin/mango install', {cwd: MANGO_DIR}, function(error, stdout, stderr) {
+exec('node node_modules/mango-cli/bin/mango install', function(error, stdout, stderr) {
 	/*if (error instanceof Error) {
 		throw error
 	}*/
 	console.log(stdout)
 })
 
-exec('node ../node_modules/mango-cli/bin/mango build', {cwd: MANGO_DIR}, function(error, stdout, stderr) {
+exec('node node_modules/mango-cli/bin/mango build', function(error, stdout, stderr) {
 	if (error instanceof Error) {
 		throw error
 	}
