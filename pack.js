a33ad1e8c9f155f52c1849d3160f472df7015d71
Filip Chalupa <chalupa.filip@gmail.com>
2016-11-15 23:17:18 +0100

    Packager configured

--- pack.js
+++ pack.js
@@ -1,30 +1,36 @@
-var asar = require('asar')
-/*var Mango = require('mango-cli')
-var MangoConfig = require('./node_modules/mango-cli/lib/helpers/config')*/
-
-
 console.log('Packing app')
-/*
-var mangoProject = new Mango(MANGO_DIR, (new MangoConfig(MANGO_DIR)).get())
-//mangoProject.install(function() {
-	mangoProject.build([], function() {
-		console.log('done')
-	})
-//})*/
 
 var exec = require('child_process').exec
 
-// @TODO: better `mango install`
-exec('node node_modules/mango-cli/bin/mango install', function(error, stdout, stderr) {
-	/*if (error instanceof Error) {
-		throw error
-	}*/
-	console.log(stdout)
-})
+var start = function() {
+	installMangoDependencies()
+}
 
-exec('node node_modules/mango-cli/bin/mango build', function(error, stdout, stderr) {
-	if (error instanceof Error) {
-		throw error
-	}
-	console.log(stdout)
-})
+// @TODO: better `mango install`
+var installMangoDependencies = function() {
+	exec('node node_modules/mango-cli/bin/mango install', function(error, stdout, stderr) {
+		console.log(stdout)
+		buildMango()
+	})
+}
+
+var buildMango = function() {
+	exec('node node_modules/mango-cli/bin/mango build', function(error, stdout, stderr) {
+		if (error instanceof Error) {
+			throw error
+		}
+		console.log(stdout)
+		createPackage()
+	})
+}
+
+var createPackage = function() {
+	exec('npm run dist', function(error, stdout, stderr) {
+		if (error instanceof Error) {
+			throw error
+		}
+		console.log(stdout)
+	})
+}
+
+start()
