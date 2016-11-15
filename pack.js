febc5b3b2ef6e73affcab1d64d377ba91c05d992
Filip Chalupa <chalupa.filip@gmail.com>
2016-11-15 21:56:09 +0100

    Pack: exec install and build

--- pack.js
+++ pack.js
@@ -1,3 +1,31 @@
 var asar = require('asar')
+/*var Mango = require('mango-cli')
+var MangoConfig = require('./node_modules/mango-cli/lib/helpers/config')*/
+
+var MANGO_DIR= 'views'
 
 console.log('Packing app')
+/*
+var mangoProject = new Mango(MANGO_DIR, (new MangoConfig(MANGO_DIR)).get())
+//mangoProject.install(function() {
+	mangoProject.build([], function() {
+		console.log('done')
+	})
+//})*/
+
+var exec = require('child_process').exec
+
+// @TODO: better `mango install`
+exec('node ../node_modules/mango-cli/bin/mango install', {cwd: MANGO_DIR}, function(error, stdout, stderr) {
+	/*if (error instanceof Error) {
+		throw error
+	}*/
+	console.log(stdout)
+})
+
+exec('node ../node_modules/mango-cli/bin/mango build', {cwd: MANGO_DIR}, function(error, stdout, stderr) {
+	if (error instanceof Error) {
+		throw error
+	}
+	console.log(stdout)
+})
