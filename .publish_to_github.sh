ebbaf62eed34f99f3fd18f0c8ec60bc7ae411b9a
Filip Chalupa <chalupa.filip@gmail.com>
2016-11-30 23:20:35 +0100

    Travis: publish

---
+++ .publish_to_github.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+GH_REPO="@github.com/Onset/git-latex.git"
+FULL_REPO="https://$GH_TOKEN$GH_REPO"
+cd dist
+git init
+git config user.email "chalupa.filip@gmail.com"
+git config user.name "Filip Chalupa"
+git add .
+git commit -m "Deploy"
+git push --force --quiet $FULL_REPO master:deploy/travis
