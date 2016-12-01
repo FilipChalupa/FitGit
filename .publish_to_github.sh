bf3f793bacfc3795bbb992447f6753859a016131
Filip Chalupa <chalupa.filip@gmail.com>
2016-12-01 02:02:36 +0100

    Deploy: package name

--- .publish_to_github.sh
+++ .publish_to_github.sh
@@ -8,4 +8,4 @@ git config user.email "chalupa.filip@gmail.com"
 git config user.name "Filip Chalupa"
 git add .
 git commit -m "Deploy"
-git push --force --quiet $FULL_REPO master:deploy/travis
+git push --force --quiet $FULL_REPO master:package/$TRAVIS_OS_NAME
