1358e6a7a296b0f72bd1726d87735d59e6358e00
Filip Chalupa <chalupa.filip@gmail.com>
2017-03-13 15:54:59 +0100

    Menu: align drawer

--- app/containers/Menu.js
+++ app/containers/Menu.js
@@ -1,26 +1,29 @@
 // @flow
-import React, { Component } from 'react'
-import { Link, browserHistory } from 'react-router'
+import React, { Component } from 'react';
+import { Link, browserHistory } from 'react-router';
 
-import AppBar from 'material-ui/AppBar'
-import Drawer from 'material-ui/Drawer'
-import MenuItem from 'material-ui/MenuItem'
-import RaisedButton from 'material-ui/RaisedButton'
+import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import RaisedButton from 'material-ui/RaisedButton';
+import IconButton from 'material-ui/IconButton';
+import NavigationClose from 'material-ui/svg-icons/navigation/close';
 
 export default class MenuPage extends Component {
 
   constructor(props) {
-    super(props)
-    this.state = {open: false}
+    super(props);
+    this.state = { open: false };
   }
 
-  handleToggle = () => this.setState({open: !this.state.open})
+  handleToggle = () => this.setState({ open: !this.state.open })
 
 
   render() {
     return (
       <div>
         <AppBar
+          style={{ position: 'sticky', top: 0 }}
           onLeftIconButtonTouchTap={this.handleToggle}
           title="Git+LaTeX"
           iconClassNameRight="muidocs-icon-navigation-expand-more"
@@ -28,9 +31,14 @@ export default class MenuPage extends Component {
 
         <Drawer
           open={this.state.open}
-          docked ={false}
-          onRequestChange={(open) => this.setState({open})}
+          docked={false}
+          onRequestChange={(open) => this.setState({ open })}
         >
+          <AppBar
+            title="Git+LaTeX"
+            iconElementLeft={<IconButton onTouchTap={() => this.setState({ open: false })}><NavigationClose /></IconButton>}
+          />
+
           <MenuItem
             containerElement={<Link to="/commit" />}
             primaryText="Commit"
