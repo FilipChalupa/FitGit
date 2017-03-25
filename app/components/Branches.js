d62b3408652892239cb6616f10916c98a092b702
Filip Chalupa <chalupa.filip@gmail.com>
2017-03-26 00:04:56 +0100

    Project: list branches

--- app/components/Branches.js
+++ app/components/Branches.js
@@ -1,15 +1,42 @@
 // @flow
 import React, { Component } from 'react'
-import {List, ListItem} from 'material-ui/List'
+import { List, ListItem } from 'material-ui/List'
+import Subheader from 'material-ui/Subheader'
 import LabelIcon from 'material-ui/svg-icons/action/label'
 import LabelOutlineIcon from 'material-ui/svg-icons/action/label-outline'
-import {blue500, red500, greenA200} from 'material-ui/styles/colors'
-import nodegit from '../utils/nodegit'
+import {
+  red500,
+  pink500,
+  purple500,
+  deepPurple500,
+  indigo500,
+  blue500,
+  green500,
+  lightGreen500,
+  lime500,
+  yellow500,
+  amber500,
+  orange500,
+  deepOrange500,
+  brown500,
+} from 'material-ui/styles/colors'
+import nodegit, { getLocalBranches, getRemoteBranches } from '../utils/nodegit'
 
 const branchColors = [
-  blue500,
   red500,
-  greenA200,
+  lightGreen500,
+  blue500,
+  yellow500,
+  pink500,
+  orange500,
+  purple500,
+  green500,
+  brown500,
+  deepOrange500,
+  deepPurple500,
+  lime500,
+  indigo500,
+  amber500,
 ]
 
 export default class Branches extends Component {
@@ -18,69 +45,94 @@ export default class Branches extends Component {
     super(props)
 
     this.state = {
-      branches: [],
+      localBranches: {},
+      remoteBranches: {},
     }
   }
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState(Object.assign({}, this.state, {
-        branches: [
-          {
-            name: 'master',
-            branches: []
-          },
-          {
-            name: 'deploy',
-            branches: [
-              {
-                name: 'prod',
-                branches: [
-                  {
-                    name: 'wroclaw',
-                    branches: [],
-                  },
-                  {
-                    name: 'sevilla',
-                    branches: [],
-                  },
-                ]
-              },
-              {
-                name: 'beta',
-                branches: []
-              },
-            ],
-          },
-        ],
-      }))
-    }, 1000)
+    getLocalBranches(this.props.project.path)
+      .then((branches) => {
+        this.setState(Object.assign({}, this.state, {
+          localBranches: this.makeTrees(branches),
+        }))
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+    getRemoteBranches(this.props.project.path)
+      .then((branches) => {
+        this.setState(Object.assign({}, this.state, {
+          remoteBranches: this.makeTrees(branches),
+        }))
+      })
+      .catch((error) => {
+        console.error(error)
+      })
   }
 
-  renderBranches(branches, prefix = '', color = null) {
-    return branches.map((branch, i) => {
+  makeTrees(branches) {
+    const trees = {}
+    branches.forEach((branch) => {
+      const parts = branch.split('/')
+      let i = 0
+      let t = trees
+      while (i < parts.length) {
+        if (t[parts[i]] === undefined) {
+          t[parts[i]] = {}
+        }
+        t = t[parts[i]]
+        i++
+      }
+    })
+    return trees
+  }
+
+  renderBranches(branches, isLocal = true, prefix = '', color = null) {
+    return branches && Object.keys(branches).map((branch, i) => {
       const groupColor = color || branchColors[i % branchColors.length]
-      const Icon = branch.branches.length ? LabelOutlineIcon : LabelIcon
+      const nestedItems = this.renderBranches(branches[branch], isLocal, `${prefix}${branch}/`, groupColor)
+      const Icon = nestedItems.length ? LabelOutlineIcon : LabelIcon
       return (
         <ListItem
           key={i}
-          primaryText={prefix + branch.name}
+          primaryText={prefix + branch}
           leftIcon={<Icon
             color={groupColor}
           />}
-          initiallyOpen={true}
-          nestedItems={this.renderBranches(branch.branches, `${prefix}${branch.name}/`, groupColor)}
+          initiallyOpen={isLocal}
+          primaryTogglesNestedList={nestedItems.length !== 0}
+          /*onTouchTap={() => {alert('@TODO')}}*/
+          nestedItems={nestedItems}
         />
       )
     })
   }
 
   render() {
+    const localBranches  = this.renderBranches(this.state.localBranches)
+    const remoteBranches = this.renderBranches(this.state.remoteBranches, false)
+
+    const localList = localBranches.length ? (
+      <div>
+        <Subheader>Lokální</Subheader>
+        {localBranches}
+      </div>
+    ) : null
+
+    const remoteList = remoteBranches.length ? (
+      <div>
+        <Subheader>Vzdálené</Subheader>
+        {remoteBranches}
+      </div>
+    ) : null
+
     return (
       <div>
         <h2>Větve</h2>
         <List>
-          {this.renderBranches(this.state.branches)}
+          {localList}
+          {remoteList}
         </List>
       </div>
     )
--- app/utils/nodegit.js
+++ app/utils/nodegit.js
@@ -3,3 +3,30 @@ import { remote } from 'electron'
 const nodegit = remote.require('nodegit')
 
 export default nodegit
+
+export function getBranches(path) {
+  return nodegit.Repository.open(path)
+    .then((repo) => {
+      return repo.getReferenceNames(nodegit.Reference.TYPE.LISTALL)
+    })
+}
+
+export function getPrefixedBranches(path, prefix) {
+  return getBranches(path)
+    .then((allBranches) => {
+      return allBranches.filter((branch) => branch.startsWith(prefix))
+    })
+    .then((branches) => {
+      return branches.map((branch) => {
+        return branch.substr(prefix.length)
+      })
+    })
+}
+
+export function getLocalBranches(path) {
+  return getPrefixedBranches(path, 'refs/heads/')
+}
+
+export function getRemoteBranches(path) {
+  return getPrefixedBranches(path, 'refs/remotes/')
+}
