699b121e6b958b8590e9a2f1fc93d175cae41ca8
Filip Chalupa <chalupa.filip@gmail.com>
2017-04-03 23:58:09 +0200

    Integrate: get hunks

--- app/components/IntegrateChanges.js
+++ app/components/IntegrateChanges.js
@@ -1,13 +1,121 @@
 // @flow
 import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import RaisedButton from 'material-ui/RaisedButton'
+import nodegit from '../utils/nodegit'
+import * as LoadingActions from '../actions/loading'
 
-export default class IntegrateChanges extends Component {
+class IntegrateChanges extends Component {
+
+
+  done() {
+    console.log('done')
+  }
+
+  accept = () => {
+    let repo
+    let localTree
+    let remoteTree
+    let patches
+    let currentPathIndex
+    let hunks
+    let currentHunkIndex
+
+    const processPatch = () => {
+      console.log('process')
+      if (patches.length === currentPathIndex) {
+        console.log('done')
+        return Promise.resolve()
+      }
+
+      return patches[currentPathIndex++].hunks()
+        .then((h) => {
+          hunks = h
+          currentHunkIndex = 0
+          processHunk()
+        })
+        .then(processPatch)
+    }
+
+    const processHunk = () => {
+      if (hunks.length === currentHunkIndex) {
+        console.log('hunks done')
+        return Promise.resolve()
+      }
+
+      console.log('Hunk')
+      console.log(hunks[currentHunkIndex].header())
+      return hunks[currentHunkIndex++].lines()
+        .then((lines) => {
+          lines.forEach((line) => {
+            console.log(line.content())
+          })
+          console.log('')
+        })
+        .then(processHunk)
+    }
+
+    this.props.actions.loading.IncrementLoadingJobs()
+    nodegit.Repository.open(this.props.projects.active.path)
+      .then((r) => {
+        repo = r
+        return repo.getBranchCommit('master') // @TODO: get main branch
+      })
+      .then((commit) => commit.getTree())
+      .then((tree) => {
+        localTree = tree
+        return repo.getBranchCommit('origin/master')
+      })
+      .then((commit) => commit.getTree())
+      .then((tree) => {
+        remoteTree = tree
+        return localTree.diff(remoteTree)
+      })
+      .then((diffs) => diffs.patches())
+      .then((p) => {
+        patches = p
+        currentPathIndex = 0
+        return processPatch()
+      })
+      .catch((error) => {
+        console.log('ups')
+        console.error(error)
+      })
+      .then(() => {
+        console.log('mega done')
+        this.props.actions.loading.DecrementLoadingJobs()
+      })
+  }
 
   render() {
     return (
       <div>
         <h1>Začlenit změny</h1>
+        <RaisedButton
+          label='Přijmout změny'
+          secondary={true}
+          onTouchTap={this.accept}
+        />
       </div>
     )
   }
 }
+
+
+function mapStateToProps(state) {
+  return {
+    loading: state.loading,
+    projects: state.projects,
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: {
+      loading: bindActionCreators(LoadingActions, dispatch),
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(IntegrateChanges)
