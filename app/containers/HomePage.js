a5f2e9331ff69531aef7cc0a7709ba0f6c4c93ca
Filip Chalupa <chalupa.filip@gmail.com>
2017-04-18 03:06:24 +0200

    Skip homepage

--- app/containers/HomePage.js
+++ app/containers/HomePage.js
@@ -2,8 +2,15 @@ const React = require('react')
 const e = React.createElement
 const Component = React.Component
 const Home = require('../components/Home')
+const hashHistory = require('react-router').hashHistory
 
 module.exports = class HomePage extends Component {
+
+	componentDidMount() {
+		hashHistory.push('/projects')
+	}
+
+
 	render() {
 		return (
 			e(Home)
