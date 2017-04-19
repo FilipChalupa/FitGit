83f0ac532eebde2b9fcfb1d866dc3fbd2b3053f4
Filip Chalupa <chalupa.filip@gmail.com>
2017-04-19 19:02:26 +0200

    Text: fallback language

--- app/utils/text.js
+++ app/utils/text.js
@@ -2,10 +2,11 @@ const loadJsonFile = require('load-json-file')
 const path = require('path')
 
 const LANGUAGES_DIR = path.resolve(__dirname, '..', 'languages')
+const DEFAULT_LANGUAGE_CODE = 'cs'
 
 const texts = {}
 
-module.exports = function text(code, key) {
+function getTranslation(code, key) {
 	if (!texts[code]) {
 		try {
 			texts[code] = loadJsonFile.sync(path.resolve(LANGUAGES_DIR, `${code}.json`))
@@ -14,5 +15,16 @@ module.exports = function text(code, key) {
 			console.error(error)
 		}
 	}
-	return texts[code][key] || '-'
+	return texts[code][key] || null
 }
+
+function text(code, key) {
+	const requestedLanguageText = getTranslation(code, key)
+	if (requestedLanguageText === null) {
+		return getTranslation(DEFAULT_LANGUAGE_CODE, key)
+	}
+	return requestedLanguageText
+}
+
+
+module.exports = text
