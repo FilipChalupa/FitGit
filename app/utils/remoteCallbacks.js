6c2e3d49a5ce312dd60867bbd9200b83399a5ef3
Filip Chalupa <chalupa.filip@gmail.com>
2017-04-24 14:30:28 +0200

    Remote callback: create fake password

--- app/utils/remoteCallbacks.js
+++ app/utils/remoteCallbacks.js
@@ -4,18 +4,26 @@ const fillSync = require('git-credential-node').fillSync
 const nodegit = require('../utils/nodegit').nodegit
 const log = require('./log')
 
+const passwords = {}
+
 module.exports = {
 	callbacks: {
 		credentials: (url, userName) => {
 			if (url.startsWith('http')) {
 				let name = ''
 				let password = ''
-				try {
-					const credentials = fillSync(url)
+				const credentials = fillSync(url)
+				if (credentials) {
 					name = credentials.username
 					password = credentials.password
-				} catch (error) {
-					log.error(error)
+				} else {
+					log.info('Credential manager not set')
+					name = userName
+					const key = `${userName}--${url}`
+					if (!passwords[key]) {
+						passwords[key] = 'fake password'
+					}
+					password = passwords[key]
 				}
 				return nodegit.Cred.userpassPlaintextNew(name, password)
 			} else {
